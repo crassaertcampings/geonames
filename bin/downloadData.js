9306faa60688fbcc97e577eeac99cf6079264b63
Cédric Rassaert <crassaert@campings.com>
2023-04-07 10:43:36 +0200

    handle multiple countries

--- bin/downloadData.js
+++ bin/downloadData.js
@@ -2,10 +2,16 @@
 
 const config = require('pelias-config').generate();
 const validateISOCode = require('../lib/validateISOCode');
-
-const countryCode = validateISOCode(config.imports.geonames.countryCode);
-
-const filename = countryCode === 'ALL' ? 'allCountries' : countryCode;
-
 const task = require('../lib/tasks/download');
-task(filename);
+let countryCodeConfig = config.imports.geonames.countryCode;
+
+if (!(countryCodeConfig instanceof Array)) {
+    countryCodeConfig = [countryCodeConfig];
+}
+
+countryCodeConfig.forEach(item => {
+    const countryCode = validateISOCode(item);
+
+    const filename = countryCode === 'ALL' ? 'allCountries' : item;
+    task(filename);
+});
--- import.js
+++ import.js
@@ -1,6 +1,7 @@
 const config = require('pelias-config').generate();
 const _ = require('lodash');
 const logger = require('pelias-logger').get('geonames');
+const dbclient = require('pelias-dbclient');
 
 if (_.has(config, 'imports.geonames.adminLookup')) {
   logger.info('imports.geonames.adminLookup has been deprecated, ' +
@@ -11,8 +12,18 @@ const resolvers = require( './lib/tasks/resolvers' );
 const task = require('./lib/tasks/import');
 const validateISOCode = require('./lib/validateISOCode');
 
-const isocode = validateISOCode( config.imports.geonames.countryCode );
-const filename = isocode === 'ALL' ? 'allCountries' : isocode;
-const source = resolvers.selectSource( filename );
+let countryCodes = config.imports.geonames.countryCode;
 
-task( source );
+if (!(countryCodes instanceof Array)) {
+  countryCodes = [countryCodes];
+}
+
+const endStream = dbclient({name: 'geonames'});
+
+countryCodes.forEach(item => {
+  const isocode = validateISOCode( item );
+  const filename = isocode === 'ALL' ? 'allCountries' : isocode;
+  const source = resolvers.selectSource( filename );
+
+  task( source, endStream );
+});
